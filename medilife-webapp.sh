7d4a2a1b25ecfcea9b144f75042517d3a87d360d
Mbandi Awan <68437700+awanmbandi@users.noreply.github.com>
2026-02-09 18:31:08 -0500

    Fix shebang and formatting in medilife-webapp.sh

--- medilife-webapp.sh
+++ medilife-webapp.sh
@@ -1,10 +1,11 @@
-"sudo apt update -y"
-"sudo apt -y install apache2"
-"sudo systemctl start apache2"
-"sudo systemctl enable apache2"
-"sudo apt install wget -y"
-"sudo wget https://github.com/awanmbandi/aws-real-world-projects/raw/web-appplications-src-code/medlife-health-care.zip -P ~/"
-"sudo apt install unzip -y" 
-"sudo unzip ~/medlife-health-care.zip"
-"sudo rm -f /var/www/html/index.html"
-"sudo cp -rf medlife-master/* /var/www/html/"
+#!/bin/bash
+sudo apt update -y
+sudo apt -y install apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+sudo apt install wget -y
+sudo wget https://github.com/awanmbandi/aws-real-world-projects/raw/web-appplications-src-code/medlife-health-care.zip -P ~/
+sudo apt install unzip -y
+sudo unzip ~/medlife-health-care.zip
+sudo rm -f /var/www/html/index.html
+sudo cp -rf medlife-master/* /var/www/html/
